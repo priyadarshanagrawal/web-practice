049b4f4a0e059a8adcc0c1ba3f5a848521e9c2d5
Priyadarshan Agrawal <113705497+priyadarshanagrawal@users.noreply.github.com>
2026-01-03 21:10:02 +0530

    Remove sayHi function and add cart functionality

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,14 @@
-function sayHi() {
-  alert("Hello! You're coding from your office PC!");
+let cart = [];
+let cartCount = 0;
+
+function addToCart(productName) {
+    cart.push(productName);
+    cartCount++;
+    document.getElementById("cart-count").innerText = cartCount;
+    alert(productName + " added to cart!");
+}
+
+// Optional: Log cart in console
+function viewCart() {
+    console.log(cart);
 }
